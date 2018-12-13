9f2ae2eb56e5684936ddc20a039989404e14fe0a
abafzal <afsanabafzal@gmail.com>
2018-12-13 12:18:33 -0500

    im so cool

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,26 +1,147 @@
 import React from "react";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
 
-export default class Welcome extends React.Component {
-  state = { data: {} };
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+});
+
+class Welcome extends React.Component {
+  state = { data: {}, search: '' };
+
+  updateSearch = (e) => {
+    this.setState({ search: e.target.value.toLowerCase() })
+  }
 
   render() {
-    const { data } = this.state;
-    // const data2 = this.state.data
+    const { data, search } = this.state;
+    const { classes } = this.props;
+
     return (
-      <div>
-        <h1>Hello, {this.props.name}</h1>
-        <p>Hey Gideon, which dentist is best? </p>
-        <List component="nav">
-          {Object.keys(data).map(name => (
-            <ListItem button key={name}>
-              <ListItemText primary={name} />
-            </ListItem>
-          ))}
-        </List>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              className={classes.title}
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
+              Material-UI
+          </Typography>
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+                value={search}
+                onChange={this.updateSearch}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+        <div>
+          <h1>Hello, {this.props.name}</h1>
+          <p style={{ textAlign: 'left' }} >Customers (1000)</p>
+          <Paper>
+            <Table >
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell numeric>Spend ($)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Object.keys(data).filter(name => name.toLowerCase().includes(search)).map(name => {
+                  return (
+                    <TableRow key={name}>
+                      <TableCell component="th" scope="row">
+                        {name}
+                      </TableCell>
+                      <TableCell numeric>{'$$$'}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </Paper>
+        </div>
       </div>
+
+
     );
   }
 
@@ -34,3 +155,5 @@ export default class Welcome extends React.Component {
       });
   }
 }
+
+export default withStyles(styles)(Welcome)
